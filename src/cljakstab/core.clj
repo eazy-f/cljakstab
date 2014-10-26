4638ccbe3b54a8bfcc53e0d9838943e7bdf1b3ae
Nikita Lebedev <eazy-f@live.com>
2014-10-26 11:45:24 +0300

    Handle System.exit()

--- src/cljakstab/core.clj
+++ src/cljakstab/core.clj
@@ -14,18 +14,36 @@
   [opts]
   (set! org.jakstab.Options/mainFilename (:mainFile opts)))
 
+(defn security-manager-proxy []
+  (proxy
+      [java.lang.SecurityManager] []
+    (checkPermission [perm]
+      (if (not (= "setSecurityManager" (.getName ^java.security.Permission perm)))
+        ;(proxy-super checkPermission perm)
+        nil))
+    (checkExit [code#]
+      (throw
+       (java.lang.SecurityException.
+        (str "catched exit with code " code#))))))
+
+(defmacro disable-system-exit [& exprs]
+  `(let [old-mgr# (java.lang.System/getSecurityManager)
+         new-mgr# (security-manager-proxy)]
+     (try
+       (do
+         (java.lang.System/setSecurityManager new-mgr#)
+         ~@exprs)
+       (finally (java.lang.System/setSecurityManager old-mgr#)))))
 
 (defn find-jumps
   "find unresolved jumps in specified binary"
   [binary-file]
   (init-options {:mainFile binary-file})
-  (let [arch (org.jakstab.ssl.Architecture. (.getValue org.jakstab.Options/sslFilename))
-        program (org.jakstab.Program/createProgram arch)
-        file (.getAbsoluteFile (java.io.File. binary-file))]
-    (.loadMainModule program file)
-    (.installHarness program (org.jakstab.loader.DefaultHarness.))
-    (try
-      (let [cfr (org.jakstab.analysis.ControlFlowReconstruction. program)]
-        (.run cfr)))))
-
-
+  (disable-system-exit
+   (let [arch (org.jakstab.ssl.Architecture. (.getValue org.jakstab.Options/sslFilename))
+         program (org.jakstab.Program/createProgram arch)
+         file (.getAbsoluteFile (java.io.File. binary-file))]
+     (.loadMainModule program file)
+     (.installHarness program (org.jakstab.loader.DefaultHarness.))
+     (let [cfr (org.jakstab.analysis.ControlFlowReconstruction. program)]
+       (.run cfr)))))
