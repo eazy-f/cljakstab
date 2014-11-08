27905c0adaeb00b1056510e3526eb5d365a01034
Nikita Lebedev <eazy-f@live.com>
2014-11-08 15:51:13 +0300

    Remove fully-qualified names of Java classes

--- src/cljakstab/core.clj
+++ src/cljakstab/core.clj
@@ -2,43 +2,46 @@
   (:import (org.jakstab Main Options Program AnalysisManager)
            (org.jakstab.ssl Architecture)
            (org.jakstab.loader DefaultHarness)
-           (org.jakstab.analysis ControlFlowReconstruction)))
+           (org.jakstab.analysis ControlFlowReconstruction)
+           java.security.Permission
+           java.io.File))
 
 (defn init-options
   "Jakstab stores options in singleton Options class"
   [opts]
-  (set! org.jakstab.Options/mainFilename (:mainFile opts))
-  (org.jakstab.AnalysisManager/getInstance))
+  (set! Options/mainFilename (:mainFile opts))
+  (AnalysisManager/getInstance))
 
 (defn security-manager-proxy []
   (proxy
-      [java.lang.SecurityManager] []
+      [SecurityManager] []
     (checkPermission [perm]
-      (if (not (= "setSecurityManager" (.getName ^java.security.Permission perm)))
+      (if (not (= "setSecurityManager" (.getName ^Permission perm)))
         nil))
     (checkExit [code]
       (throw
-       (java.lang.SecurityException.
+       (SecurityException.
         (str "catched exit with code " code))))))
 
 (defmacro disable-system-exit [& exprs]
-  `(let [old-mgr# (java.lang.System/getSecurityManager)
+  `(let [old-mgr# (System/getSecurityManager)
          new-mgr# (security-manager-proxy)]
      (try
        (do
-         (java.lang.System/setSecurityManager new-mgr#)
+         (System/setSecurityManager new-mgr#)
          ~@exprs)
-       (finally (java.lang.System/setSecurityManager old-mgr#)))))
+       (finally (System/setSecurityManager old-mgr#)))))
 
 (defn find-jumps
   "find unresolved jumps in specified binary"
   [binary-file]
   (init-options {:mainFile binary-file})
   (disable-system-exit
-   (let [arch (org.jakstab.ssl.Architecture. (.getValue org.jakstab.Options/sslFilename))
-         program (org.jakstab.Program/createProgram arch)
-         file (.getAbsoluteFile (java.io.File. binary-file))]
+   (let [arch (Architecture. (.getValue Options/sslFilename))
+         program (Program/createProgram arch)
+         file (.getAbsoluteFile (File. binary-file))]
      (.loadMainModule program file)
-     (.installHarness program (org.jakstab.loader.DefaultHarness.))
-     (let [cfr (org.jakstab.analysis.ControlFlowReconstruction. program)]
-       (.run cfr)))))
+     (.installHarness program (DefaultHarness.))
+     (let [cfr (ControlFlowReconstruction. program)]
+       (.run cfr)
+       cfr))))
